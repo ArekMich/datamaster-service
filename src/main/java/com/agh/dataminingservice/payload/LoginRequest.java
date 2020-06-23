8809da27dad4580c818d88947621f30f1e093a81
ArekMich <arkadiusz.michalik96@gmail.com>
2020-06-23 08:46:11 +0200

    Add default message for LoginRequest

--- src/main/java/com/agh/dataminingservice/payload/LoginRequest.java
+++ src/main/java/com/agh/dataminingservice/payload/LoginRequest.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class LoginRequest {
 
-    @NotBlank
+    @NotBlank(message = "Field with usernameOrEmail could not be blank.")
     private String usernameOrEmail;
 
-    @NotBlank
+    @NotBlank(message = "Field with password could not be blank.")
     private String password;
 }
